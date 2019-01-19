79d2c5723e114b3af9f7e072495689baa9bd2165
Eric Elliott <eric.elliott@tseboston.com>
2019-01-18 22:09:29 -0500

    updated elm related files

--- assets/elm/main.elm
+++ assets/elm/main.elm
@@ -1,37 +1,120 @@
+module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
+
 import Browser
-import Html exposing (Html, div, text, h1)
+import Html exposing (Html, a, div, h1, nav, text)
+import Html.Attributes exposing (..)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
 
 
 -- Model
 
-type alias Model = 
-  { 
-    ticker: String
-  }
 
-model : Model
-model = 
-  {
-    ticker = "BTC/USD"
-  }
+type alias Model =
+    { ticker : String
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { ticker = "BTC/USD"
+      }
+    , Cmd.none
+    )
+
+
 
 -- Update
 
-type Msg
-  = ToUpper
-  | ToLower
 
-update msg m =
-  case msg of
-    ToUpper ->
-      (String.toUpper m.ticker)
-    ToLower ->
-      (String.toLower m.ticker)
+type Msg
+    = ToUpper
+    | ToLower
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ToUpper ->
+            ( { ticker = String.toUpper model.ticker }
+            , Cmd.none
+            )
+
+        ToLower ->
+            ( { ticker = String.toLower model.ticker }, Cmd.none )
+
+
 
 -- View
 
+
 view : Model -> Html Msg
-view m = 
-  div []
-      [ div [] [ text ToUpper ]
-      ]
+view model =
+    nav
+        [ class "navbar"
+        , class "is-primary"
+        , attribute "role" "navigation"
+        , attribute "aria-label" "main navigation"
+        ]
+        [ div [ class "navbar-brand" ]
+            [ a
+                [ class "navbar-item"
+                , attribute "href" "/"
+                ]
+                [ text "Cointool" ]
+            , a
+                [ class "navbar-burger"
+                , attribute "role" "button"
+                , attribute "aria-label" "menu"
+                , attribute "aria-expanded" "false"
+                , attribute "data-target" "mainNavbar"
+                ]
+                []
+            ]
+        , div
+            [ id "mainNavbar"
+            , class "navbar-menu"
+            ]
+            [ div [ class "navbar-start" ]
+                [ div
+                    [ class "navbar-item"
+                    , class "has-dropdown"
+                    , class "is-hoverable"
+                    ]
+                    [ div [ class "navbar-link" ]
+                        [ a
+                            [ attribute "href" "/tools/" ]
+                            [ text "Tools" ]
+                        ]
+                    , div [ class "navbar-dropdown" ]
+                        [ a
+                            [ class "navbar-item"
+                            , attribute "href" "/tools/psc"
+                            ]
+                            [ text "Position Size Calculator" ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -38,8 +38,9 @@ module.exports = (env, options) => ({
       {
         test: /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
-        use: {
-          loader: 'elm-webpack-loader'
+        loader: 'elm-webpack-loader',
+        options: {
+          debug: true
         }
       },
       {
